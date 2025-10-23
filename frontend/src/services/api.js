3f6972bfebdd1fa3c0abb931bdcca45c795d3162
Fernando Calisaya <205468351+Fe25dev@users.noreply.github.com>
2025-10-23 00:06:51 -0300

    v1 proyecto iniciado

---
+++ frontend/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001",
+});
+
+export default api;
+
+
+// //para listar alertas:
+
+// export async function getAlertas(cuartelId) {
+//   const res = await api.get(`/alertas/${cuartelId}`);
+//   return res.data;
+// }
